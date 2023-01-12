210598b8fa14cc04c05b1f45559d5fcb99823a9a
LinLinLinWang <1339567630@qq.com>
2023-01-12 15:54:41 +0800

    重写

--- 图解算法/Algu.cpp
+++ 图解算法/Algu.cpp
@@ -22,11 +22,77 @@ void gcd(int a, int b) {
 }
 
 
-int main() {
+/*int main() {
 	gcd(3,5);
 	gcd(5,3);
 	gcd(28,14);
 	gcd(4, 56);
 	return 0;
 
+}*/
+
+
+
+
+
+
+//∫œ≤¢≈≈–Ú
+void mergePart(int *a, int low, int middle, int n) {
+	int i = low, j = middle;
+	int k = 0;
+	int* b = new int[n];
+	while (i<=middle&&j<=n-1) {
+		if (a[i] <= a[j]) {
+			b[k] = a[i];
+			i++;
+			k++;
+		  
+		}
+		else {
+			b[k] = a[j];
+			k++;
+			j++;
+		}
+	
+	
+	
+	}
+	while (j<=n-1) {
+
+		
+			b[k] = a[j];
+			j++;
+			k++;
+		
+	
+	}
+	while(i<=middle)
+	
+	{
+			b[k] = a[i];
+			k++;
+			i++;
+		
+	}
+
+	for (int t = 0; t <= n - 1;t++) {
+		a[t] = b[t];
+	
+	}
+
 }
+
+
+int main() {
+	int a[] = { 10,9,8,5,2 };
+	mergePart(a,0,2,5);
+
+
+	return 0;
+}
+
+
+
+
+
+
