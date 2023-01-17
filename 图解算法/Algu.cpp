863fbf6c325e581dc94f33c64a9a9ec9f4dc325e
LinLinLinWang <1339567630@qq.com>
2023-01-17 15:15:10 +0800

    完善合并算法

--- 图解算法/Algu.cpp
+++ 图解算法/Algu.cpp
@@ -37,56 +37,59 @@ void gcd(int a, int b) {
 
 
 //∫œ≤¢≈≈–Ú
-void mergePart(int *a, int low, int middle, int n) {
-	int i = low, j = middle;
+void mergePart(int* a, int low, int middle, int n) {
+	int i = low, j = middle + 1;
 	int k = 0;
-	int* b = new int[n];
-	while (i<=middle&&j<=n-1) {
+	int* b = new int[n - low + 1];
+	while (i <= middle && j <= n) {
 		if (a[i] <= a[j]) {
 			b[k] = a[i];
 			i++;
 			k++;
-		  
+
 		}
 		else {
 			b[k] = a[j];
 			k++;
 			j++;
 		}
-	
-	
-	
-	}
-	while (j<=n-1) {
 
-		
-			b[k] = a[j];
-			j++;
-			k++;
-		
-	
+
+
 	}
-	while(i<=middle)
-	
+	while (j <= n) {
+
+
+		b[k] = a[j];
+		j++;
+		k++;
+
+
+	}
+	while (i <= middle)
+
 	{
-			b[k] = a[i];
-			k++;
-			i++;
-		
+		b[k] = a[i];
+		k++;
+		i++;
+
 	}
 
-	for (int t = 0; t <= n - 1;t++) {
-		a[t] = b[t];
-	
-	}
+	for (i = low, k = 0; i <= n; i++) {
+		a[i] = b[k++];
 
+	}
+	delete[]b;
 }
 
+	//
 
-int main() {
+
+int main(){
+
 	int a[] = { 10,9,8,5,2 };
-	mergePart(a,0,2,5);
 
+	mergePart(a,0,2,4);
 
 	return 0;
 }
